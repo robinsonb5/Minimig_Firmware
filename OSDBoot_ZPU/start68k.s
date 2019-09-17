79a18c01c32bb361dcb3651b6568a502a114ba47
Alastair M. Robinson <robinsonb5@gmail.com>
2019-09-17 22:43:27 +0100

    Fixed reset problem with bootloader

--- OSDBoot_ZPU/start68k.s
+++ OSDBoot_ZPU/start68k.s
@@ -1,8 +1,3 @@
-	section .fixed_vectors
-	dc.l	stack_top
-	dc.l	Start
-	ds.l	62,0
-
 	section .text
 
 Start:
@@ -17,22 +12,6 @@ l1
 	clr.l   (a0)+
 	bra.s   l1
 l2
-	; Set up interrupt vectors...
-	lea	.int1,a0
-	move.l	a0,$64
-	lea	.int2,a0
-	move.l	a0,$68
-	lea	.int3,a0
-	move.l	a0,$6C
-	lea	.int4,a0
-	move.l	a0,$70
-	lea	.int5,a0
-	move.l	a0,$74
-	lea	.int6,a0
-	move.l	a0,$78
-	lea	.int7,a0
-	move.l	a0,$7C
-
 	; Startup code complete, hand control over to C code.
 	pea 1
 	pea	.name
@@ -40,54 +19,24 @@ l2
 .loop
 	bra.s .loop
 
+	; Set up interrupt vectors...
+;	lea	.int1,a0
+;	move.l	a0,$64
+;	lea	.int2,a0
+;	move.l	a0,$68
+;	lea	.int3,a0
+;	move.l	a0,$6C
+;	lea	.int4,a0
+;	move.l	a0,$70
+;	lea	.int5,a0
+;	move.l	a0,$74
+;	lea	.int6,a0
+;	move.l	a0,$78
+;	lea	.int7,a0
+;	move.l	a0,$7C
+
 .name
 	dc.b	"Bootrom",0
-
-.int1:
-	movem.l	d0-7/a0-6,-(a7)	; Preserve scratch registers
-	pea	.intend1	; A stub to restore the scratch registers
-	move.l	IntHandler1,-(a7)	; equivalent to move.l IntHandler1,reg; jmp reg - without using registers
-	rts
-
-.int2:
-	movem.l	d0-7/a0-6,-(a7)	; Preserve scratch registers
-	pea	.intend1
-	move.l	IntHandler2,-(a7)
-	rts
-
-.int3:
-	movem.l	d0-7/a0-6,-(a7)	; Preserve scratch registers
-	pea	.intend1
-	move.l	IntHandler3,-(a7)
-	rts
-
-.int4:
-	movem.l	d0-7/a0-6,-(a7)	; Preserve scratch registers
-	pea	.intend1
-	move.l	IntHandler4,-(a7)
-	rts
-
-.int5
-	movem.l	d0-7/a0-6,-(a7)	; Preserve scratch registers
-	pea	.intend1
-	move.l	IntHandler5,-(a7)
-	rts
-
-.int6
-	movem.l	d0-7/a0-6,-(a7)	; Preserve scratch registers
-	pea	.intend1
-	move.l	IntHandler6,-(a7)
-	rts
-
-.int7
-	movem.l	d0-7/a0-6,-(a7)	; Preserve scratch registers
-	pea	.intend1
-	move.l	IntHandler7,-(a7)
-	rts
-
-.intend1
-	movem.l	(a7)+,d0-d7/a0-a6
-	rte
 
 DummyIntHandler
 	rts
@@ -126,4 +75,16 @@ DisableInterrupts
 	XDEF IntHandler5
 	XDEF IntHandler6
 	XDEF IntHandler7
+
+	section .fixed_vectors
+	dc.l	stack_top
+	dc.l	Start
+	ds.b	$5c,0
+	dc.l	DummyIntHandler
+	dc.l	DummyIntHandler
+	dc.l	DummyIntHandler
+	dc.l	DummyIntHandler
+	dc.l	DummyIntHandler
+	dc.l	DummyIntHandler
+	dc.l	DummyIntHandler
 
